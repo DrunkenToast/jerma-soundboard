0f19e8abb153aa0a221e795e228d2489ac61464f
DrunkenToast <peter5leconte@gmail.com>
2022-03-25 10:32:11 +0100

    toolbar

--- app/src/main/java/com/example/appdevproject/MainActivity.kt
+++ app/src/main/java/com/example/appdevproject/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.appdevproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.popup_menu, menu)
+        return true
+    }
+
 }
