f1bf8b913e7400fba759eb53f348662829df37fb
DrunkenToast <peter5leconte@gmail.com>
2022-03-31 16:47:31 +0200

    open settings activity

--- app/src/main/java/com/example/appdevproject/MainActivity.kt
+++ app/src/main/java/com/example/appdevproject/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.appdevproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,4 +19,14 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
 }
