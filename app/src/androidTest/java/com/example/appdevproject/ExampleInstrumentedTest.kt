cb74eaa550ab85355762cc0f042a14fec0634345
Peter Leconte <peter5leconte@gmail.com>
2022-12-27 21:01:33 +0100

    Unit test recyclerview

--- app/src/androidTest/java/com/example/appdevproject/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/appdevproject/ExampleInstrumentedTest.kt
@@ -1,12 +1,29 @@
 package com.example.appdevproject
 
+import android.content.res.Resources
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.appdevproject.audioList.AudioAdapter
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +32,79 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule var mActivityScenarioRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun startMainActivity() {
+        Espresso.onView(withId(R.id.rv_audio_list))
+            .perform(
+                RecyclerViewActions
+                    .scrollToPosition<AudioAdapter.AudioViewHolder>(0)
+            )
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.appdevproject", appContext.packageName)
+    fun testFirstItemTitle() {
+        // Check if the first item has the correct title
+        Espresso
+            .onView(RecyclerViewMatcher(R.id.rv_audio_list)
+            .atPositionOnView(0, R.id.audio_title))
+            .check(matches(withText("AA")))
+    }
+}
+
+class RecyclerViewMatcher(private val recyclerViewId: Int) {
+
+    fun atPosition(position: Int): Matcher<View> {
+        return atPositionOnView(position, -1)
+    }
+
+    fun atPositionOnView(position: Int, targetViewId: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            var resources: Resources? = null
+            var childView: View? = null
+
+            override fun describeTo(description: Description) {
+                var idDescription = recyclerViewId.toString()
+                if (this.resources != null) {
+                    idDescription = try {
+                        this.resources!!.getResourceName(recyclerViewId)
+                    } catch (var4: Resources.NotFoundException) {
+                        String.format(
+                            "%s (resource name not found)",
+                            recyclerViewId
+                        )
+                    }
+
+                }
+
+                description.appendText("with id: $idDescription")
+            }
+
+            override fun matchesSafely(view: View): Boolean {
+                this.resources = view.resources
+
+                if (childView == null) {
+                    val recyclerView =
+                        view.rootView.findViewById<View>(recyclerViewId) as RecyclerView
+                    if (recyclerView.id == recyclerViewId) {
+                        childView =
+                            recyclerView.findViewHolderForAdapterPosition(position)?.itemView
+                    } else {
+                        return false
+                    }
+                }
+
+                return if (targetViewId == -1) {
+                    view === childView
+                } else {
+                    val targetView = childView!!.findViewById<View>(targetViewId)
+                    view === targetView
+                }
+
+            }
+        }
     }
 }
